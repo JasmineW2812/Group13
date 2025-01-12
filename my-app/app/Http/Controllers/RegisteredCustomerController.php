8109f7400ca62f66f3a205e0beb1a9423c2dfc7d
c3052701 <c3052701@hallam.shu.ac.uk>
2025-01-12 23:05:01 +0000

    update

--- my-app/app/Http/Controllers/RegisteredCustomerController.php
+++ my-app/app/Http/Controllers/RegisteredCustomerController.php
@@ -11,35 +11,35 @@ class RegisteredCustomerController extends Controller
 {
     public function customerSignUp(Request $request){
 
-    $validated = $request->validate([
-        'first_name' => 'required|string|max:255',
-        'last_name'  => 'required|string|max:255',
-        'email'      => 'required|email|unique:registered_customers,email',
-        'phone_number'=> 'required|integer',
-        'password'   => 'required|string|min:6|confirmed',
-        ]);
-    
-    DB::beginTransaction();
-    
-    try {
-        $newUser = RegisteredCustomer::create([
-            'first_name' => $validated['first_name'],
-            'last_name'  => $validated['last_name'],
-            'email'      => $validated['email'],
-            'phone_number'      => $validated['phone_number'],
-            'password'   => Hash::make($validated['password']),
-        ]);
-
-        DB::commit();
-
-        return redirect()->route('customerSignUp')->with('success', 'Your account has been created successfully!');
-    } 
-    catch (\Exception $e) {
-        DB::rollback();
+        $validated = $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name'  => 'required|string|max:255',
+            'email'      => 'required|email|unique:registered_customers,email',
+            'phone_number'=> 'required|integer',
+            'password'   => 'required|string|min:6|confirmed',
+            ]);
         
-        return redirect()->back()->withErrors(['error' => 'Please try again.']);
+        DB::beginTransaction();
+    
+        try {
+            $newUser = RegisteredCustomer::create([
+                'first_name' => $validated['first_name'],
+                'last_name'  => $validated['last_name'],
+                'email'      => $validated['email'],
+                'phone_number'      => $validated['phone_number'],
+                'password'   => Hash::make($validated['password']),
+            ]);
+
+            DB::commit();
+
+            return redirect()->route('customerSignUp')->with('success', 'Your account has been created successfully!');
+        } 
+        catch (\Exception $e) {
+            DB::rollback();
+            
+            return redirect()->back()->withErrors(['error' => 'Please try again.']);
+        }
     }
-}
 
 
     /**

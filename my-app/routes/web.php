c0462ca338cb7308baf02f7296d7c125269d029a
A-G0 <147385967+A-G0@users.noreply.github.com>
2025-01-14 08:42:18 +0000

    .

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', [HomeController::class, 'home'])->name('home');
 
-Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.detail');
-
-Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.list');
-
 Route::get('/admin-create-staff', function () {
     return view('admin-create-staff');
 })->name('admin.createStaff');
